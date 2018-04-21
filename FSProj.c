1c983d46f9f9ea422d8b17706e9d473abc6ac8cc
santiweight <sweight19@cmc.edu>
2018-04-20 18:02:42 -0700

    read is better with offsets

--- FSProj.c
+++ FSProj.c
@@ -1,4 +1,3 @@
-<<<<<<< current
 /*
   studentfs: STUDENT FILE SYSTEM
   Authors: Jess Winssinger, Evan Chrisinger, Santi Weight
@@ -36,7 +35,9 @@
 #include <sys/time.h>
 #include <sys/param.h>
 #include <string.h>
+#ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
+#endif
 
 /* Project header files */
 #include "consts.h"
@@ -133,15 +134,13 @@ char *get_next_vnum(const char *path) {
 		return "1";
 	}
 
+	#ifdef HAVE_SETXATTR
 	int res = studentfs_getxattr(path, CURR_VNUM, curr_vnum, MAX_VNUM_LEN);
 	if (res < 0) {
 		printf("Error getting xattr %s, error is presumably that the wrong file was passed\n", CURR_VNUM);
 		exit(0);
 	}
-<<<<<<< HEAD
 	#endif
-=======
->>>>>>> origin/jess
 
 	return _get_next_vnum(path, curr_vnum);
 }
@@ -495,10 +494,7 @@ static int studentfs_create(const char *path, mode_t mode, struct fuse_file_info
 
 static int studentfs_open(const char *path, struct fuse_file_info *fi)
 {
-<<<<<<< HEAD
 	#ifdef HAVE_SETXATTR
-=======
->>>>>>> origin/jess
 	int fd;
 	int create_flag = (fi->flags & O_CREAT) == O_CREAT;
 	char *sdir_str = malloc(sizeof(SDIR_XATTR));
@@ -527,7 +523,20 @@ static int studentfs_open(const char *path, struct fuse_file_info *fi)
 	}
 	fi->fh = fd;
 	free(sdir_str);
+	#endif
 	return 0;
+}
+
+static int read_offset(char *path, char *buf, size_t size, off_t offset){
+	int fd = open(path);
+	int res;
+
+	res = lseek(fd, offset, SEEK_SET);
+	if (res == -1)
+		return res;
+		
+	res = read(fd, buf, size);
+	return res;
 }
 
 static int studentfs_read(const char *path, char *buf, size_t size, off_t offset,
@@ -535,7 +544,7 @@ static int studentfs_read(const char *path, char *buf, size_t size, off_t offset
 {
 	int res;
 	char* vnum = malloc(MAX_VNUM_LEN);
-	char*new_path = malloc(2*MAX_VNUM_LEN);
+	char* new_path = malloc(2*MAX_VNUM_LEN);
 	char temp[sizeof(SDIR_XATTR)];
 
 	int is_sdir = getxattr(path, SDIR_XATTR, temp, size(SDIR_XATTR));
@@ -543,7 +552,7 @@ static int studentfs_read(const char *path, char *buf, size_t size, off_t offset
 		res = chmod(path, 755 | S_IFDIR);
 		if (res == -1)
 			return -errno;
-		res = getxattr(path, VNUM_XATTR, vnum, MAX_VNUM_LEN);
+		res = getxattr(path, CURR_VNUM, vnum, MAX_VNUM_LEN);
 		if(res == -1)
 			return -errno;
 
@@ -551,7 +560,8 @@ static int studentfs_read(const char *path, char *buf, size_t size, off_t offset
 		strcat(new_path, "/");
 		strcat(new_path, vnum);
 
-		res = read(new_path, buf, size, offset, fi);
+
+		res = read_offset(new_path, buf, size, offset);
 		if (res == -1)
 			return -errno;
 
@@ -560,9 +570,11 @@ static int studentfs_read(const char *path, char *buf, size_t size, off_t offset
 		return res;
 	}
 	else{
-		res = read(path, buf, size, offset, fi);
+		res = read_offset(path, buf, size, offset);
 		return res;
 	}
+	free(vnum);
+	free(new_path);
 
 }
 
@@ -596,6 +608,7 @@ static int studentfs_write(const char *path, const char *buf, size_t size,
 	if (getxattr(path, SDIR_XATTR, val, 0) >= 0) {
 
 		char *curr_ver = malloc(MAX_VNUM_LEN);
+		char *next_ver = malloc(MAX_VNUM_LEN);
 		char *new_path = malloc(2*MAX_VNUM_LEN);
 
 
@@ -618,16 +631,9 @@ static int studentfs_write(const char *path, const char *buf, size_t size,
 		 * Make number of changes before creating a new version file specific (xattr)
 		 * Use diff to compute the differences instead of using the size of the write
 		 */
-<<<<<<< HEAD
 		if (write_diff && (write_diff + size > 2*MAX_NO_CHANGES)) {
-
-=======
-		if (ver_changes(fi->fh) && (ver_changes(fi->fh) + size > 2*MAX_NO_CHANGES)) {
-			/*
-			 * Write two files if there were previous changes and the new changes on top
-			 * of the old changes will go over the size of the maximum number of changes.
-			 */
->>>>>>> origin/jess
+			next_ver = get_next_vnum(path);
+			str_cpy(nextpath,
 
 		} else if (ver_changes(fi->fh) + size > MAX_NO_CHANGES) {
 			/*
@@ -650,588 +656,6 @@ static int studentfs_write(const char *path, const char *buf, size_t size,
 	res = pwrite(fi->fh, buf, size, offset);
 	if (res == -1)
 		res = -errno;
-
-	return res;
-}
-
-static int studentfs_write_buf(const char *path, struct fuse_bufvec *buf,
-		     off_t offset, struct fuse_file_info *fi)
-{
-	struct fuse_bufvec dst = FUSE_BUFVEC_INIT(fuse_buf_size(buf));
-
-	(void) path;
-
-	dst.buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;
-	dst.buf[0].fd = fi->fh;
-	dst.buf[0].pos = offset;
-
-	return fuse_buf_copy(&dst, buf, FUSE_BUF_SPLICE_NONBLOCK);
-}
-
-static int studentfs_statfs(const char *path, struct statvfs *stbuf)
-{
-	int res;
-
-	res = statvfs(path, stbuf);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_flush(const char *path, struct fuse_file_info *fi)
-{
-	int res;
-
-	(void) path;
-	/* This is called from every close on an open file, so call the
-	   close on the underlying filesystem.	But since flush may be
-	   called multiple times for an open file, this must not really
-	   close the file.  This is important if used on a network
-	   filesystem like NFS which flush the data/metadata on close() */
-	res = close(dup(fi->fh));
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_release(const char *path, struct fuse_file_info *fi)
-{
-	(void) path;
-	close(fi->fh);
-
-	return 0;
-}
-
-static int studentfs_fsync(const char *path, int isdatasync,
-		     struct fuse_file_info *fi)
-{
-	int res;
-	(void) path;
-
-#ifndef HAVE_FDATASYNC
-	(void) isdatasync;
-#else
-	if (isdatasync)
-		res = fdatasync(fi->fh);
-	else
-#endif
-		res = fsync(fi->fh);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-/* xattr operations are optional and can safely be left unimplemented */
-static int studentfs_setxattr(const char *path, const char *name, const char *value,
-			size_t size, int flags)
-{
-	int res = lsetxattr(path, name, value, size, flags);
-	if (res == -1)
-		return -errno;
-	return 0;
-}
-
-static int studentfs_getxattr(const char *path, const char *name, char *value,
-			size_t size)
-{
-	int res = lgetxattr(path, name, value, size);
-
-	if (res == -1)
-		return -errno;
-	return res;
-}
-
-static int studentfs_listxattr(const char *path, char *list, size_t size)
-{
-	int res = llistxattr(path, list, size);
-
-	if (res == -1)
-		return -errno;
-	return res;
-}
-
-static int studentfs_removexattr(const char *path, const char *name)
-{
-	int res = lremovexattr(path, name);
-
-	if (res == -1)
-		return -errno;
-	return 0;
-}
-
-void *
-studentfs_init(struct fuse_conn_info *conn)
-{
-	return NULL;
-}
-
-void
-studentfs_destroy(void *userdata)
-{
-}
-
-
-static struct fuse_operations studentfs_oper = {
-	.init	   	= studentfs_init,
-	.destroy	= studentfs_destroy,
-	.getattr	= studentfs_getattr,
-	.fgetattr	= studentfs_fgetattr,
-	.access		= studentfs_access,
-	.readlink	= studentfs_readlink,
-	.opendir	= studentfs_opendir,
-	.readdir	= studentfs_readdir,
-	.releasedir	= studentfs_releasedir,
-	.mknod		= studentfs_mknod,
-	.mkdir		= studentfs_mkdir,
-	.symlink	= studentfs_symlink,
-	.unlink		= studentfs_unlink,
-	.rmdir		= studentfs_rmdir,
-	.rename		= studentfs_rename,
-	.link		= studentfs_link,
-	.chmod		= studentfs_chmod,
-	.chown		= studentfs_chown,
-	.truncate	= studentfs_truncate,
-	.ftruncate	= studentfs_ftruncate,
-#ifdef HAVE_UTIMENSAT
-	.utimens	= studentfs_utimens,
-#endif
-	.create		= studentfs_create,
-	.open		= studentfs_open,
-	.read		= studentfs_read,
-	.read_buf	= studentfs_read_buf,
-	.write		= studentfs_write,
-	.write_buf	= studentfs_write_buf,
-	.statfs		= studentfs_statfs,
-	.flush		= studentfs_flush,
-	.release	= studentfs_release,
-	.fsync		= studentfs_fsync,
-	.setxattr	= studentfs_setxattr,
-	.getxattr	= studentfs_getxattr,
-	.listxattr	= studentfs_listxattr,
-	.removexattr	= studentfs_removexattr,
-	.flag_nullpath_ok = 1,
-#if HAVE_UTIMENSAT
-	.flag_utime_omit_ok = 1,
-#endif
-};
-
-int main(int argc, char *argv[])
-{
-	umask(0);
-	return fuse_main(argc, argv, &studentfs_oper, NULL);
-}
-=======
-/*
-  studentfs: STUDENT FILE SYSTEM
-  Authors: Jess Winssinger, Evan Chrisinger, Santi Weight
-
-  Source: fuse-2.8.7.tar.gz examples directory
-  	Used as FUSE basis for our features to be built on top of Unix.
-  	Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-  	Copyright (C) 2011       Sebastian Pipping <sebastian@pipping.org>
-
-	fusexmp_fh.c can be distributed under the terms of the GNU GPL.
-  gcc -Wall studentfs.c `pkg-config fuse --cflags --libs` -o studentfs
-*/
-
-#define FUSE_USE_VERSION 26
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef __APPLE__
-#define _DARWIN_C_SOURCE
-#else
-#define _GNU_SOURCE
-#endif
-
-#include <fuse.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <errno.h>
-#include <sys/time.h>
-#ifdef HAVE_SETXATTR
-#include <sys/xattr.h>
-#endif
-
-#include <sys/param.h>
-
-typedef struct Sdir {
-	char* ver_num; 	  //< Version number
-	uint64_t sfh; 	  //< File handle of snapfile
-	mode_t smode;	  //< Mode for snapfile
-	size_t ssize;	  //< Size of snapfile
-	size_t size;	  //< ssize + size of subtree
-	size_t entry_cnt; //< # of entries
-} Sdir;
-
-typedef struct SuperSdir {
-	char* fname;    	//< Name of file
-	size_t scount;		//< # of snapshots
-	char* curr_ver;		//< Current version
-	uint64_t curr_fh;	//< Current version file handle
-} SuperSdir;
-
-static int studentfs_getattr(const char *path, struct stat *stbuf)
-{
-	int res;
-
-	res = lstat(path, stbuf);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_fgetattr(const char *path, struct stat *stbuf,
-			struct fuse_file_info *fi)
-{
-	int res;
-
-	(void) path;
-
-	res = fstat(fi->fh, stbuf);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_access(const char *path, int mask)
-{
-	int res;
-
-	res = access(path, mask);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_readlink(const char *path, char *buf, size_t size)
-{
-	int res;
-
-	res = readlink(path, buf, size - 1);
-	if (res == -1)
-		return -errno;
-
-	buf[res] = '\0';
-	return 0;
-}
-
-struct studentfs_dirp {
-	DIR *dp;
-	struct dirent *entry;
-	off_t offset;
-};
-
-static int studentfs_opendir(const char *path, struct fuse_file_info *fi)
-{
-	int res;
-	struct studentfs_dirp *d = malloc(sizeof(struct studentfs_dirp));
-	if (d == NULL)
-		return -ENOMEM;
-
-	d->dp = opendir(path);
-	if (d->dp == NULL) {
-		res = -errno;
-		free(d);
-		return res;
-	}
-	d->offset = 0;
-	d->entry = NULL;
-
-	fi->fh = (unsigned long) d;
-	return 0;
-}
-
-static inline struct studentfs_dirp *get_dirp(struct fuse_file_info *fi)
-{
-	return (struct studentfs_dirp *) (uintptr_t) fi->fh;
-}
-
-static int studentfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
-{
-	struct studentfs_dirp *d = get_dirp(fi);
-
-	(void) path;
-	if (offset != d->offset) {
-		seekdir(d->dp, offset);
-		d->entry = NULL;
-		d->offset = offset;
-	}
-	while (1) {
-		struct stat st;
-		off_t nextoff;
-
-		if (!d->entry) {
-			d->entry = readdir(d->dp);
-			if (!d->entry)
-				break;
-		}
-
-		memset(&st, 0, sizeof(st));
-		st.st_ino = d->entry->d_ino;
-		st.st_mode = d->entry->d_type << 12;
-		nextoff = telldir(d->dp);
-		if (filler(buf, d->entry->d_name, &st, nextoff))
-			break;
-
-		d->entry = NULL;
-		d->offset = nextoff;
-	}
-
-	return 0;
-}
-
-static int studentfs_releasedir(const char *path, struct fuse_file_info *fi)
-{
-	struct studentfs_dirp *d = get_dirp(fi);
-	(void) path;
-	closedir(d->dp);
-	free(d);
-	return 0;
-}
-
-static int studentfs_mknod(const char *path, mode_t mode, dev_t rdev)
-{
-	int res;
-
-	if (S_ISFIFO(mode))
-		res = mkfifo(path, mode);
-	else
-		res = mknod(path, mode, rdev);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_mkdir(const char *path, mode_t mode)
-{
-	int res;
-
-	res = mkdir(path, mode);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_unlink(const char *path)
-{
-	int res;
-
-	res = unlink(path);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_rmdir(const char *path)
-{
-	int res;
-
-	res = rmdir(path);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_symlink(const char *from, const char *to)
-{
-	int res;
-
-	res = symlink(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_rename(const char *from, const char *to)
-{
-	int res;
-
-	res = rename(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_link(const char *from, const char *to)
-{
-	int res;
-
-	res = link(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_chmod(const char *path, mode_t mode)
-{
-	int res;
-	res = chmod(path, mode);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_chown(const char *path, uid_t uid, gid_t gid)
-{
-	int res;
-
-	res = lchown(path, uid, gid);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_truncate(const char *path, off_t size)
-{
-	int res;
-
-	res = truncate(path, size);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int studentfs_ftruncate(const char *path, off_t size,
-			 struct fuse_file_info *fi)
-{
-	int res;
-
-	(void) path;
-
-	res = ftruncate(fi->fh, size);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-#ifdef HAVE_UTIMENSAT
-static int studentfs_utimens(const char *path, const struct timespec ts[2])
-{
-	int res;
-
-	/* don't use utime/utimes since they follow symlinks */
-	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-#endif
-
-static int studentfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
-{
-	int fd;
-
-	fd = open(path, fi->flags, mode);
-	if (fd == -1)
-		return -errno;
-
-	fi->fh = fd;
-	return 0;
-}
-
-static int studentfs_open(const char *path, struct fuse_file_info *fi)
-{
-	int fd;
-
-	fd = open(path, fi->flags);
-	if (fd == -1)
-		return -errno;
-
-	fi->fh = fd;
-	return 0;
-}
-
-static int studentfs_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
-{
-	int res;
-	char* vnum = malloc(MAX_VNUM_LEN);
-	char*new_path = malloc(2*MAX_VNUM_LEN);
-
-	res = chmod(path, 755 | S_IFDIR);
-	if (res == -1)
-		return -errno;
-	res = getxattr(path, VNUM_XATTR, vnum, MAX_VNUM_LEN);
-	if(res == -1)
-		return -errno;
-
-	strcpy(new_path, path);
-	strcat(new_path, "/");
-	strcat(new_path, vnum);
-
-	res = read(new_path, buf, size, offset, fi);
-
-	return res;
-}
-
-static int studentfs_read_buf(const char *path, struct fuse_bufvec **bufp,
-			size_t size, off_t offset, struct fuse_file_info *fi)
-{
-	struct fuse_bufvec *src;
-
-	(void) path;
-
-	src = malloc(sizeof(struct fuse_bufvec));
-	if (src == NULL)
-		return -ENOMEM;
-
-	*src = FUSE_BUFVEC_INIT(size);
-
-	src->buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;
-	src->buf[0].fd = fi->fh;
-	src->buf[0].pos = offset;
-
-	*bufp = src;
-
-	return 0;
-}
-
-static int studentfs_write(const char *path, const char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi)
-{
-	int res;
-	char* vnum = malloc(MAX_VNUM_LEN);
-	char*new_path = malloc(2*MAX_VNUM_LEN);
-
-	res = chmod(path, 755 | S_IFDIR);
-	if (res == -1)
-		return -errno;
-	res = getxattr(path, VNUM_XATTR, vnum, MAX_VNUM_LEN);
-	if(res == -1)
-		return -errno;
-
-	strcpy(new_path, path);
-	strcat(new_path, "/");
-	strcat(new_path, vnum);
-
-	res = read(new_path, buf, size, offset, fi);
 
 	return res;
 }
@@ -1357,6 +781,7 @@ studentfs_destroy(void *userdata)
 {
 }
 
+
 static struct fuse_operations studentfs_oper = {
 	.init	   	= studentfs_init,
 	.destroy	= studentfs_destroy,
@@ -1408,4 +833,3 @@ int main(int argc, char *argv[])
 	umask(0);
 	return fuse_main(argc, argv, &studentfs_oper, NULL);
 }
->>>>>>> before discard
