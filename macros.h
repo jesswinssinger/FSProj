53db9350b88aee2ba0566bc837ec7622bfa6a343
santiweight <sweight19@cmc.edu>
2018-04-24 22:43:49 -0700

    Revert "macros/consts upgrade"
    This reverts commit 1192bbedcaa50590dd526ea8857bd648c422816c.

--- macros.h
+++ macros.h
@@ -1,5 +1,6 @@
 #include "consts.h"
 
+#define is_sdir_ftype(path) !strcmp(path+strlen(path)-strlen(SDIR_FILETYPE), SDIR_FILETYPE)
 <<<<<<< HEAD
 =======
 #define is_snap(path) !strcmp(path+strlen(path)-strlen(VER_SUFFIX), VER_SUFFIX)
