1ebefa3f03bb94b1aaa022d16b35f8a86f9bddc7
Evan Chrisinger <echrisinger@gmail.com>
2018-05-06 16:59:12 -0700

    small changes

--- scripts/mk_sdir.sh
+++ scripts/mk_sdir.sh
@@ -4,7 +4,7 @@ if [ -e $1 ]; then
 else
     if [ "$#" -eq 1 ]; then
         echo -1 > "/tmp/SDIR_info"
-        mkdir ".$1.SDIR;-1"
+        mkdir ".$1.SDIR"
     elif [ "$#" -eq 2 ]; then
         echo $2 > "/tmp/SDIR_info"
         mkdir ".$1.SDIR"
